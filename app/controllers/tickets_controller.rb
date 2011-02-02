4d6393be71d3822913dd6b9ab129885d8be78e28
Ben Holley <benolee@gmail.com>
2011-02-02 15:16:58 -0600

    [TICKETS] remove duplicate authorization method from controller, remove duplicate edit action

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -2,7 +2,7 @@ class TicketsController < ApplicationController
   before_filter :load_new_ticket, :only => [:new, :create]
   before_filter :load_ticket, :only => [:show, :edit, :update]
   before_filter :load_file_attachments, :only => [:show, :new, :create]
-  before_filter :require_access
+  before_filter :load_project
 
   access_control do
     allow :admin
@@ -10,43 +10,11 @@ class TicketsController < ApplicationController
     allow :client, :of => :project, :to => :show
   end
 
-  protected
-
-  def load_new_ticket
-    @ticket = Ticket.new(params[:ticket])
-    @ticket.project = Project.find(params[:project_id]) if params[:project_id]
-  end
-
-  def load_ticket
-    @ticket = Ticket.find(params[:id])
-  end
-
-  def load_file_attachments
-    @file_attachments = @ticket.file_attachments
-  end
-
-  public
-
+  # GET /tickets/new
   def new
   end
 
-  def show
-    @work_units = WorkUnit.for_ticket(@ticket).sort_by_scheduled_at
-  end
-
-  def edit
-  end
-
-  def update
-    if @ticket.update_attributes(params[:ticket])
-      flash[:notice] = t(:ticket_updated_successfully)
-      redirect_to ticket_path(@ticket)
-    else
-      flash.now[:error] = t(:ticket_updated_unsuccessfully)
-      render :action => 'edit'
-    end
-  end
-
+  # POST /tickets
   def create
     if @ticket.save
       if request.xhr?
@@ -63,16 +31,44 @@ class TicketsController < ApplicationController
     end
   end
 
+  # GET /tickets/:id
+  def show
+    @work_units = WorkUnit.for_ticket(@ticket).sort_by_scheduled_at
+  end
+
+  # GET /tickets/:id/edit
   def edit
+  end
+
+  # PUT /tickets/:id
+  def update
+    if @ticket.update_attributes(params[:ticket])
+      flash[:notice] = t(:ticket_updated_successfully)
+      redirect_to ticket_path(@ticket)
+    else
+      flash.now[:error] = t(:ticket_updated_unsuccessfully)
+      render :action => 'edit'
+    end
   end
 
   private
 
-  def require_access
-    unless @ticket.allows_access? current_user
-      flash[:notice] = "Access denied."
-      redirect_to root_path
+    def load_new_ticket
+      @ticket = Ticket.new(params[:ticket])
+      @ticket.project = Project.find(params[:project_id]) if params[:project_id]
     end
-  end
+
+    def load_ticket
+      @ticket = Ticket.find(params[:id])
+    end
+
+    def load_file_attachments
+      @file_attachments = @ticket.file_attachments
+    end
+
+    # Acl9's access control block looks for @project in order to use "allow <role> :of => :project"
+    def load_project
+      @project = @ticket.project
+    end
 
 end
