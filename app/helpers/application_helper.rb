e59b35f376058d8d67e4185ef834030c4683f026
Adam Dill <altrux@gmail.com>
2011-03-10 23:57:45 -0600

    [HELPERS] added a pair of function to simplify views, these generate the externally billed work percentage and the last effective date for calculating target hours offset

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,19 @@ module ApplicationHelper
     end
   end
 
+  def external_work_percentage(user, start_date, end_date)
+    if @site_settings.client
+      internal = user.percentage_work_for(@site_settings.client, start_date, end_date)
+      (100 - internal).to_s
+    end
+  end
+
+  def last_effective_date(start_date)
+    if start_date.end_of_week > Date.current 
+      Date.current
+    else
+      start_date.end_of_week
+    end
+  end
+
 end
