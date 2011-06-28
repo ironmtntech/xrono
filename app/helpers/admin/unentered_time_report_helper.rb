510002aa792e1af15235b1165d2dc3b553355abc
Kiba <hackerkiba@gmail.com>
2011-06-28 11:49:23 -0500

    take argument time

--- app/helpers/admin/unentered_time_report_helper.rb
+++ app/helpers/admin/unentered_time_report_helper.rb
@@ -1,8 +1,8 @@
 module Admin::UnenteredTimeReportHelper
-  def hours_entered_for_day
+  def hours_entered_for_day time
     hours = 0
     @users.each do |u|
-      hours += u.hours_entered_for_day(Time.now)
+      hours += u.hours_entered_for_day(time)
     end
     hours
   end
