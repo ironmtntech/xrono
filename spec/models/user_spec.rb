b88babfaa36044edefb2916fee828c9c6f6bc6d3
Kiba <hackerkiba@gmail.com>
2011-06-30 14:44:59 -0500

    should validate the presence of expandable

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,7 +9,7 @@ describe User do
 
   it { should validate_presence_of :first_name }
   it { should validate_presence_of :last_name }
-
+  it { should validate_presence_of :expandable }
   describe '.with_unpaid_work_units' do
     subject { User.with_unpaid_work_units }
 
