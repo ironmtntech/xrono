bec62c9335295710c3f88452287b80152ead547d
Kiba <hackerkiba@gmail.com>
2011-07-05 11:20:34 -0500

    rename expandable to expanded_calendar

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me,
                   :first_name, :last_name, :middle_initial, :full_width,
-                  :daily_target_hours, :expandable
+                  :daily_target_hours, :expanded_calendar
 
   validates_presence_of :first_name, :last_name
   validates_length_of :middle_initial, :is => 1
