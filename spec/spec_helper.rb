514489da17d5d644fe4975853f0537c8d97c4cc2
Kiba <hackerkiba@gmail.com>
2011-07-14 09:24:08 -0500

    use spork

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,38 +1,54 @@
+require 'spork'
+
+Spork.prefork do
+  # Loading more in this block will cause your tests to run faster. However,
+  # if you change any configuration or code from libraries loaded here, you'll
+  # need to restart spork for it take effect.
+  # This file is copied to spec/ when you run 'rails generate rspec:install'
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+  require File.expand_path(File.dirname(__FILE__) + "/blueprints")
+  require 'rspec/rails'
+  require 'devise/test_helpers'
+
+  # Requires supporting ruby files with custom matchers and macros, etc,
+  # in spec/support/ and its subdirectories.
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+
+  RSpec.configure do |config|
+    # == Mock Framework
+    #
+    # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+    #
+    # config.mock_with :mocha
+    # config.mock_with :flexmock
+    # config.mock_with :rr
+    config.mock_with :rspec
+
+    config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+    # Reset the shams and such before all/each run
+    config.before(:all)    { Sham.reset(:before_all)  }
+    config.before(:each)   { Sham.reset(:before_each) }
+
+    # If you're not using ActiveRecord, or you'd prefer not to run each of your
+    # examples within a transaction, comment the following line or assign false
+    # instead of true.
+    config.use_transactional_fixtures = true
+  end
+  
+end
+
+Spork.each_run do
+  # This code will be run each time you run your specs.
+
+end
+
+
 # Simplecov Setup
 require 'simplecov'
 SimpleCov.start 'rails' do
   add_filter '/vendor/'
 end
 
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require File.expand_path(File.dirname(__FILE__) + "/blueprints")
-require 'rspec/rails'
-require 'devise/test_helpers'
-
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
-
-RSpec.configure do |config|
-  # == Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-  config.mock_with :rspec
-
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # Reset the shams and such before all/each run
-  config.before(:all)    { Sham.reset(:before_all)  }
-  config.before(:each)   { Sham.reset(:before_each) }
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, comment the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
-end
