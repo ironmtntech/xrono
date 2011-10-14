0254817bc5ddf7a23eda1fd5c7d5fec7660e0e01
Tad Hosford <tad@isotope11.com>
2011-10-14 14:59:17 -0500

    Deleted a js function call to a non existing func

--- public/javascripts/add_taxonomy.js
+++ public/javascripts/add_taxonomy.js
@@ -216,7 +216,6 @@ function get_week(week){
       $('.week_pagination').html(response.week_pagination);
       // We have to reload the triggers since they are regenerated on each request
       set_week_pagination_triggers();
-      set_trigger_for_work_unit();
     }
   });
 }
