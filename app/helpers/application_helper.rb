de7bbf27d7d249c5cbac2239aa1174301db91784
Josh Adams <josh@isotope11.com>
2011-11-07 21:24:31 -0600

    updated more pages to use bootstrap classes, modified xrono logo to fit in topbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,4 +24,21 @@ module ApplicationHelper
     end
   end
 
+  def render_label_for(hour_type)
+    label_type = nil
+    case hour_type
+    when 'Overtime'
+      label_type = 'important'
+    when 'PTO'
+      label_type = 'success'
+    when 'CTO'
+      label_type = 'warning'
+    end
+    if label_type
+      haml_tag(:div, :class => 'label ' << label_type) do
+        haml_concat hour_type
+      end
+    end
+  end
+
 end
