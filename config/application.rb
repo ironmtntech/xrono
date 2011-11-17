01de10d23e31c0d2ab211bea1d4d14b29c98da34
Josh Adams <josh@isotope11.com>
2011-11-16 22:07:30 -0600

    Set default time zone to central time.

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module AssetTrackerTutorial
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Central Time (US & Canada)'
 
     config.generators do |g|
       g.template_engine :haml
