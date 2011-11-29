4df821b70938f7d726129f04751d49637162c165
Adam Gamble <adamgamble@gmail.com>
2011-11-29 09:53:20 -0600

    db schema

--- db/schema.rb
+++ db/schema.rb
@@ -136,7 +136,6 @@ ActiveRecord::Schema.define(:version => 20110927192744) do
   create_table "users", :force => true do |t|
     t.string   "email",                               :default => "",    :null => false
     t.string   "encrypted_password",   :limit => 128, :default => "",    :null => false
-    t.string   "password_salt",                       :default => "",    :null => false
     t.string   "reset_password_token"
     t.string   "remember_token"
     t.datetime "remember_created_at"
