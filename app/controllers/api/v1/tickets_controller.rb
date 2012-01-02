8b6052cd415546f9d95c45661fed94ee88050af5
Josh Adams <josh@isotope11.com>
2012-01-02 14:26:10 -0600

    lol 500

--- app/controllers/api/v1/tickets_controller.rb
+++ app/controllers/api/v1/tickets_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::TicketsController < Api::V1::BaseController
   def index
     @bucket = Ticket
 
-    @bucket = @bucket.find(:all, :conditions => {:project_id => params[:project_id]}, :order => "name")
+    @bucket = @bucket.where(:project_id => params[:project_id])
 
     @tickets = @bucket.order("name").all
     render :json => @tickets
