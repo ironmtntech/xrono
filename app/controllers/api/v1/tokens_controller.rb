2c3af5357fba2580ad16d5f3b1f1fd2422ef398f
Adam Gamble <adamgamble@gmail.com>
2012-01-03 16:39:21 -0600

    [AG] added user information to the json returned by the tokens api controller

--- app/controllers/api/v1/tokens_controller.rb
+++ app/controllers/api/v1/tokens_controller.rb
@@ -11,7 +11,15 @@ class Api::V1::TokensController < Api::V1::BaseController
     @user.ensure_authentication_token!
 
     if @user.valid_password?(params["password"])
-      render :json => {:token => @user.authentication_token, :success => true} and return
+      json_hash = {
+        :token          => @user.authentication_token,
+        :success        => true,
+        :current_hours  => @user.unpaid_work_units.sum(:effective_hours),
+        :pto_hours      => @user.pto_hours_left(Time.zone.now.end_of_week),
+        :offset         => @user.target_hours_offset(last_effective_date(start_date)),
+        :admin          => @user.admin?
+      }
+      render :json => json_hash and return
     else
       render :json => {:success => false} and return
     end
