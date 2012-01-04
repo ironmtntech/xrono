512cefadaaa9b713e9bd4169f877671a94a36bee
Adam Gamble <adamgamble@gmail.com>
2012-01-04 14:03:03 -0600

    [AG] charting is done on a per user level

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -5,18 +5,19 @@ module DashboardHelper
     chart_color = options.fetch(:chart_color, "F9F9F9")
     date        = options.fetch(:date, Time.zone.now)
     title       = options.fetch(:title, "")
-    internal_hours, external_hours, sum_hours = [0],[0],[0]
+    internal_hours, external_hours = [nil],[nil]
     #Step through each day in the week and build an array of internal and external hours
     (date.beginning_of_week.to_date..date.end_of_week.to_date).each do |i_date|
       internal_hours << user.work_units.scheduled_between(i_date.beginning_of_day,i_date.end_of_day).for_client(@site_settings.client).sum(:hours)
       external_hours << user.work_units.scheduled_between(i_date.beginning_of_day,i_date.end_of_day).except_client(@site_settings.client).sum(:hours)
-      sum_hours      << internal_hours.last + external_hours.last
     end
+    internal_hours.map!{|hours| hours == 0 ? nil : hours}
+    external_hours.map!{|hours| hours == 0 ? nil : hours}
     image_tag Gchart.line( :title => title,
                            :size => width,
-                           :data => [internal_hours,external_hours,sum_hours],
+                           :data => [internal_hours,external_hours],
                            :line_colors => "FF0000,00FF00,0FFFFF",
-                           :legend => ["Internal","External","Total"],
+                           :legend => ["Internal","External"],
                            :background => chart_color,
                            :axis_with_labels => ["x","y"],
                            :axis_labels => ["|Mon|Tue|Wed|Thu|Fri|Sat|Sun"])
