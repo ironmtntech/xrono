baab41e185c6b2493ab3cf35e7b3e7d9b00a833f
Adam Gamble <adamgamble@gmail.com>
2012-01-04 12:29:37 -0600

    [AG] added graph to dashboard

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,18 @@
 module DashboardHelper
+
+  def external_hours_chart user, date = Time.zone.now
+    internal_hours, external_hours, sum_hours = [0],[0],[0]
+    (date.beginning_of_week.to_date..date.end_of_week.to_date).each do |i_date|
+      internal_hours << user.work_units.scheduled_between(i_date.beginning_of_day,i_date.end_of_day).for_client(@site_settings.client).sum(:hours)
+      external_hours << user.work_units.scheduled_between(i_date.beginning_of_day,i_date.end_of_day).except_client(@site_settings.client).sum(:hours)
+      sum_hours      << internal_hours.last + external_hours.last
+    end
+    image_tag Gchart.line( :title => "Hours This Week",
+                           :size => "450x150",
+                           :data => [internal_hours,external_hours,sum_hours],
+                           :line_colors => "FF0000,00FF00,0FFFFF",
+                           :legend => ["Internal","External","Total"],
+                           :axis_with_labels => ["x","y"],
+                           :axis_labels => ["|Mon|Tue|Wed|Thu|Fri|Sat|Sun"])
+  end
 end
--- app/models/work_unit.rb
+++ app/models/work_unit.rb
@@ -11,6 +11,7 @@ class WorkUnit < ActiveRecord::Base
   scope :unpaid, lambda{ where('paid IS NULL or paid = ""') }
   scope :not_invoiced, lambda{ where('invoiced IS NULL OR invoiced = ""') }
   scope :for_client, lambda{|client| joins({:ticket => {:project => [:client]}}).where("clients.id = ?", client.id) }
+  scope :except_client, lambda{|client| joins({:ticket => {:project => [:client]}}).where("clients.id <> ?", client.id) }
   scope :for_project, lambda{|project| joins({:ticket => [:project]}).where("projects.id = ?", project.id)}
   scope :for_ticket, lambda {|ticket| where(:ticket_id => ticket.id) }
   scope :for_user, lambda{|user| where(:user_id => user.id)}
