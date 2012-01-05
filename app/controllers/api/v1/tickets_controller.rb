f634569b79413bed351453fac3debb1ace713ad9
Adam Gamble <adamgamble@gmail.com>
2012-01-05 17:17:54 -0600

    [AG] fixed 500

--- app/controllers/api/v1/tickets_controller.rb
+++ app/controllers/api/v1/tickets_controller.rb
@@ -17,7 +17,7 @@ class Api::V1::TicketsController < Api::V1::BaseController
     if @ticket.percentage_complete
       @pie_chart_url = Gchart.pie(:title => "Remaining Estimated Hours vs Hours Worked -- #{@ticket.percentage_complete}% complete",
         :size => '450x230', :bar_color => "76959c,364447", :data => [100 - @ticket.percentage_complete, @ticket.percentage_complete], :labels => ["Remaining Estimated", "Hours Worked"])
-      json_hash[:pie_chart_url]  => @pie_chart_url
+      json_hash[:pie_chart_url] = @pie_chart_url
     end
     render :json => json_hash
   end
