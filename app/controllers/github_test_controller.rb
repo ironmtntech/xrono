ee145dd592a94cc3eb225b2ea5160ee3ecc376b8
Adam Gamble <adamgamble@gmail.com>
2012-01-04 21:06:24 -0600

    [AG] removed github controller

--- app/controllers/github_test_controller.rb
+++
@@ -1,7 +0,0 @@
-class GithubTestController < ApplicationController
-  skip_before_filter :authenticate_user!
-  def payload
-    gp = GitPush.create(:payload => JSON.parse(params["payload"]))
-    render :nothing => true, :status => 200
-  end
-end
