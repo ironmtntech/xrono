2d4a8b3ebb6dafcf927b5f575aa79a7400a3c0fe
Adam Gamble <adamgamble@gmail.com>
2012-01-05 17:13:56 -0600

    [AG] added a test to make sure ticket show works

--- app/controllers/api/v1/tickets_controller.rb
+++ app/controllers/api/v1/tickets_controller.rb
@@ -11,12 +11,14 @@ class Api::V1::TicketsController < Api::V1::BaseController
   def show
     @ticket = Ticket.find params[:id]
 
-    @pie_chart_url = Gchart.pie(:title => "Remaining Estimated Hours vs Hours Worked -- #{@ticket.percentage_complete}% complete",
-      :size => '450x230', :bar_color => "76959c,364447", :data => [100 - @ticket.percentage_complete, @ticket.percentage_complete], :labels => ["Remaining Estimated", "Hours Worked"])
     json_hash = {
         :name           => @ticket.name,
-        :pie_chart_url  => @pie_chart_url
     }
+    if @ticket.percentage_complete
+      @pie_chart_url = Gchart.pie(:title => "Remaining Estimated Hours vs Hours Worked -- #{@ticket.percentage_complete}% complete",
+        :size => '450x230', :bar_color => "76959c,364447", :data => [100 - @ticket.percentage_complete, @ticket.percentage_complete], :labels => ["Remaining Estimated", "Hours Worked"])
+      json_hash[:pie_chart_url]  => @pie_chart_url
+    end
     render :json => json_hash
   end
 end
