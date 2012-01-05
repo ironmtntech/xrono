cd8ad04d063388b8b83b0d13a8db72de1665d440
Adam Gamble <adamgamble@gmail.com>
2012-01-05 16:39:01 -0600

    [AG] added ticket show api

--- app/controllers/api/v1/tickets_controller.rb
+++ app/controllers/api/v1/tickets_controller.rb
@@ -7,4 +7,16 @@ class Api::V1::TicketsController < Api::V1::BaseController
     @tickets = @bucket.order("name").all
     render :json => @tickets
   end
+
+  def show
+    @ticket = Ticket.find params[:id]
+
+    @pie_chart_url = Gchart.pie(:title => "Remaining Estimated Hours vs Hours Worked -- #{@ticket.percentage_complete}% complete",
+      :size => '450x230', :bar_color => "76959c,364447", :data => [100 - @ticket.percentage_complete, @ticket.percentage_complete], :labels => ["Remaining Estimated", "Hours Worked"])
+    json_hash = {
+        :name           => @ticket.name,
+        :pie_chart_url  => @pie_chart_url
+    }
+    render :json => json_hash
+  end
 end
