1363d65704bd6a2d28b0538b87a7683a8071ea82
Josh Adams <josh@isotope11.com>
2012-01-12 09:59:09 -0600

    use a single partial for showing tickets on a given project, and use the same partial in client_login namespace.

--- app/controllers/client_login/projects_controller.rb
+++ app/controllers/client_login/projects_controller.rb
@@ -11,19 +11,16 @@ class ClientLogin::ProjectsController < ClientLogin::BaseController
   # GET /projects/:id
   def show
     @tickets = Ticket.for_project(@project).sort_by_name
-    # FIXME - not showing correct group of work_unitsl
+    # FIXME - not showing correct group of work_units
     @work_units = Project.find(params[:id]).work_units
   end
 
-
   private
+  def load_project
+    @project = Project.find(params[:id])
+  end
 
-    def load_project
-      @project = Project.find(params[:id])
-    end
-
-    def load_file_attachments
-      @file_attachments = @project.file_attachments
-    end
-
+  def load_file_attachments
+    @file_attachments = @project.file_attachments
+  end
 end
