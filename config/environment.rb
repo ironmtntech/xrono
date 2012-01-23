2a577b4765dc22b6e46c0cfaaf1b3c7461196c44
David Chapman <david@isotope11.com>
2012-01-23 23:22:20 +0000

    Load requirements as first initializer.

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 AssetTrackerTutorial::Application.initialize!
-
-#This is a hack so that github integration works.
-#Take that lazy loading!
-[Project,Ticket]
