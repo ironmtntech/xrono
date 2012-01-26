e573084bfe1cf3972a1c484702cd760a5040f9f2
Adam Gamble <adamgamble@gmail.com>
2012-01-26 10:48:14 -0600

    Cleaned up clients projects into one partial and action

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,6 +1,6 @@
 class ClientsController < ApplicationController
   before_filter :load_new_client, :only => [:new, :create]
-  before_filter :load_client, :only => [:edit, :show, :show_completed, :update]
+  before_filter :load_client, :only => [:edit, :show, :update]
   before_filter :load_file_attachments, :only => [:show, :new, :create]
 
   access_control do
@@ -75,19 +75,6 @@ class ClientsController < ApplicationController
     else
       @incompleted_projects = Project.order("name").incomplete.for_client(@client).for_user(current_user)
       @completed_projects = Project.order("name").complete.for_client(@client).for_user(current_user)
-    end
-  end
-
-  def show_complete
-    @client = Client.find params[:client_id]
-    if admin?
-      @completed_projects = Project.order("name").complete.for_client(@client)
-    else
-      if @client.allows_access? current_user
-        @completed_projects = Project.order("name").complete.for_client(@client).for_user(current_user)
-      else
-        access_denied
-      end
     end
   end
 
