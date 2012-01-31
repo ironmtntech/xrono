1e27e3a03969a78e02ecd0f61eab5e5396119ee1
Adam Gamble <adamgamble@gmail.com>
2012-01-30 21:56:34 -0600

    IT WORKS\!

---
+++ spec/controllers/api/v1/projects_controller.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe Api::V1::ProjectsController do
+
+  before(:each) do
+    @user = User.make
+    request.env['warden'].stub :authenticate! => @user
+    controller.stub :current_user => @user
+    @project = Project.make
+    @project_2 = Project.make
+    @project_3 = Project.make
+    @user.ensure_authentication_token!
+    @user.has_role!("developer", @project)
+    @user.has_role!("client", @project_2)
+  end
+
+  describe "when getting index as a non admin" do
+    it "should return all @projects that you have a role for" do
+      response = get :index, :auth_token => @user.authentication_token
+      response.body.should == [@project_2,@project].sort{|a,b| a.name <=> b.name}.to_json
+    end
+
+    it "should filter by client_id" do
+      response = get :index, :auth_token => @user.authentication_token, :client_id => @project_2.client.id
+      response.body.should == [@project_2].to_json
+    end
+
+    it "should filter by client initials" do
+      response = get :index, :auth_token => @user.authentication_token, :client_initials => @project_2.client.initials
+      response.body.should == [@project_2].to_json
+    end
+
+    it "should filter by git repo url" do
+      @project_2.update_attribute(:git_repo_url, "test")
+      response = get :index, :auth_token => @user.authentication_token, :git_repo_url => "test"
+      response.body.should == [@project_2].to_json
+      @project_2.update_attribute(:git_repo_url, nil)
+    end
+  end
+
+  describe "when getting index as an admin" do
+    it "should return all @projects that you have a role for" do
+      @user.has_role!("admin")
+      response = get :index, :auth_token => @user.authentication_token
+      response.body.should == [@project_3,@project,@project_2].sort{|a,b| a.name <=> b.name}.to_json
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,6 +29,7 @@ Spork.prefork do
     # config.mock_with :flexmock
     # config.mock_with :rr
     config.mock_with :rspec
+    config.include Devise::TestHelpers, :type => :controller
 
     config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
