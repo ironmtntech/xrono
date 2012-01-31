3a85f47c85a07a68803d1e7a6f4de4e327a6df88
Adam Gamble <adamgamble@gmail.com>
2012-01-30 23:22:56 -0600

    test coverage\!

---
+++ spec/controllers/api/v1/tickets_controller_spec.rb
@@ -0,0 +1,90 @@
+require 'spec_helper'
+
+describe Api::V1::TicketsController do
+
+  before(:each) do
+    @user = User.make
+    request.env['warden'].stub :authenticate! => @user
+    controller.stub :current_user => @user
+    @ticket = Ticket.make
+    @ticket.project.update_attribute(:git_repo_url, "test_2")
+    @ticket.update_attribute(:git_branch, "test_branch")
+
+    @ticket_1 = Ticket.make
+    @ticket_1.project.update_attribute(:git_repo_url, "test")
+    @ticket_1.update_attribute(:git_branch, "test_branch")
+  end
+
+  describe "when I hit show" do
+    describe "with a valid id" do
+      it "should return that ticket" do
+        response = get :show, :id => @ticket.id
+        json_hash = ""
+        [@ticket].sort{|a,b| a.name <=> b.name}.each do |ticket|
+          json_hash = {
+              :name                 => ticket.name,
+              :estimated_hours      => ticket.estimated_hours,
+              :percentage_complete  => ticket.percentage_complete,
+              :hours                => ticket.hours
+          }
+        end
+        response.body.should == json_hash.to_json
+      end
+    end
+  end
+  describe "when I hit index" do
+    describe "without any parameters" do
+      it "should return all tickets" do
+        response = get :index
+        json_array = []
+        [@ticket,@ticket_1].sort{|a,b| a.name <=> b.name}.each do |ticket|
+          json_hash = {
+              :id                   => ticket.id,
+              :name                 => ticket.name,
+              :estimated_hours      => ticket.estimated_hours,
+              :percentage_complete  => ticket.percentage_complete,
+              :hours                => ticket.hours
+          }
+          json_array << json_hash
+        end
+        response.body.should == json_array.to_json
+      end
+    end
+    describe "with some parameters" do
+      describe "git_repo_url and branch" do
+        it "should return all tickets from that project with git_repo_url and branch_name" do
+          response = get :index, :repo_url => "test", :branch => "test_branch"
+          json_array = []
+          [@ticket_1].sort{|a,b| a.name <=> b.name}.each do |ticket|
+            json_hash = {
+                :id                   => ticket.id,
+                :name                 => ticket.name,
+                :estimated_hours      => ticket.estimated_hours,
+                :percentage_complete  => ticket.percentage_complete,
+                :hours                => ticket.hours
+            }
+            json_array << json_hash
+          end
+          response.body.should == json_array.to_json
+        end
+      end
+      describe "project_id" do
+        it "should return all tickets from that project" do
+          response = get :index, :project_id => @ticket.project_id
+          json_array = []
+          [@ticket].sort{|a,b| a.name <=> b.name}.each do |ticket|
+            json_hash = {
+                :id                   => ticket.id,
+                :name                 => ticket.name,
+                :estimated_hours      => ticket.estimated_hours,
+                :percentage_complete  => ticket.percentage_complete,
+                :hours                => ticket.hours
+            }
+            json_array << json_hash
+          end
+          response.body.should == json_array.to_json
+        end
+      end
+    end
+  end
+end
