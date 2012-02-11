b2614fc4b4683774aa95491db61067af02068404
Adam Gamble <adamgamble@gmail.com>
2012-02-11 01:07:27 -0600

    refactored date monkey patch

--- lib/date_ext.rb
+++ lib/date_ext.rb
@@ -1,20 +1,22 @@
 class Date
 
   def prev_working_day
-    _prev_day = self - 1
-    if [6,7].include? _prev_day.cwday
-      _prev_day.prev_working_day
-    else
-      _prev_day
-    end
+    _day = self - 1
+    determine_working_day :prev_working_day, _day
   end
 
   def next_working_day
-    _next_day = self + 1
-    if [6,7].include? _next_day.cwday
-      _next_day.next_working_day
+    _day = self + 1
+    determine_working_day :next_working_day, _day
+  end
+
+  private
+
+  def determine_working_day method, _day
+    if [6,7].include? _day.cwday
+      _day.send(method)
     else
-      _next_day
+      _day
     end
   end
 
