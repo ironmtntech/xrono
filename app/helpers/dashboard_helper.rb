792e0dc61bfdc91a5083f9be05d4936d2bf56171
Josh Adams <josh@isotope11.com>
2012-02-26 16:14:05 -0600

    argh syntax

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,6 +1,6 @@
 module DashboardHelper
   def render_message(message)
-    if message do
+    if message
       content_tag('div', :title => message[:title], :style => 'display: none') do
         content_tag('p', message[:body])
       end
