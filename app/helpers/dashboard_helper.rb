120a1db99a453db62e94579ddeef62ff6d7f107e
Josh Adams <josh@isotope11.com>
2012-02-26 16:43:19 -0600

    try to fix message helper one more time....grr

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,7 +1,7 @@
 module DashboardHelper
   def render_message(message)
     if message
-      content_tag('div', :title => message[:title], :style => 'display: none') do
+      content_tag('div', :id => 'message', :title => message[:title], :style => 'display: none;') do
         content_tag('p', message[:body])
       end
     end
