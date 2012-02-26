c78eb4dee9094378244f5cda86c93df50056f5e4
Josh Adams <josh@isotope11.com>
2012-02-26 16:04:38 -0600

    Fix bug I introduced when factoring out the message helper

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,5 +1,9 @@
 module DashboardHelper
   def render_message(message)
-    content_tag('p', message[:body]) if message
+    if message do
+      content_tag('div', :title => message[:title], :style => 'display: none') do
+        content_tag('p', message[:body])
+      end
+    end
   end
 end
