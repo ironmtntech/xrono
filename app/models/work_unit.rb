c4dcb900e3f17c922950114293f43d2bf46a5886
Josh Adams <josh@isotope11.com>
2012-02-26 17:23:30 -0600

    Use breadcrumbs and button groups

--- app/models/work_unit.rb
+++ app/models/work_unit.rb
@@ -120,7 +120,11 @@ class WorkUnit < ActiveRecord::Base
   end
 
   def to_s
-    description
+    if new_record?
+      I18n.t(:new_work_unit)
+    else
+      description[0..80]
+    end
   end
 
   def allows_access?(user)
