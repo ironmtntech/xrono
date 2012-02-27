114ceeda9c13e7257d9f09797ce790361c4832f9
Philip Arndt <parndt@gmail.com>
2012-02-27 20:17:42 +1300

    Converted @ variables to let syntax for clients_controller_spec.rb

--- spec/controllers/api/v1/clients_controller_spec.rb
+++ spec/controllers/api/v1/clients_controller_spec.rb
@@ -2,15 +2,16 @@ require 'spec_helper'
 
 describe Api::V1::ClientsController do
 
+  let(:user) { User.make }
+  let(:project) { Project.make }
+  let(:project_1) { Project.make }
+
   before(:each) do
-    @user = User.make
-    request.env['warden'].stub :authenticate! => @user
-    controller.stub :current_user => @user
-    @user.ensure_authentication_token!
-    @project = Project.make
-    @project_1 = Project.make
-    @user.has_role!("developer", @project)
-    @user.has_role!("developer", @project_1)
+    request.env['warden'].stub :authenticate! => user
+    controller.stub :current_user => user
+    user.ensure_authentication_token!
+    user.has_role!("developer", project)
+    user.has_role!("developer", project_1)
   end
 
   describe "when I hit index" do
@@ -23,15 +24,15 @@ describe Api::V1::ClientsController do
 
     describe "without any parameters except the auth token" do
       it "returns all clients I have a role for" do
-        response = get :index, :auth_token => @user.authentication_token
-        response.body.should == [@project.client, @project_1.client].sort{|a,b| a.name <=> b.name}.to_json
+        response = get :index, :auth_token => user.authentication_token
+        response.body.should == [project.client, project_1.client].sort{|a,b| a.name <=> b.name}.to_json
       end
     end
 
     describe "with initials" do
       it "returns all clients I have a role for that have the intials I sent" do
-        response = get :index, :initials => @project.client.initials, :auth_token => @user.authentication_token
-        response.body.should == [@project.client].to_json
+        response = get :index, :initials => project.client.initials, :auth_token => user.authentication_token
+        response.body.should == [project.client].to_json
       end
     end
   end
