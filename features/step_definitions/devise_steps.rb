5dbc4916362441593ebdd55b8a2fd10509a4cc0b
Ben Holley <ben@isotope11.com>
2012-03-08 14:15:17 -0600

    [jruby] separate cucumber steps for jruby

--- features/step_definitions/devise_steps.rb
+++ features/step_definitions/devise_steps.rb
@@ -1,4 +1,4 @@
-Given /^I am an authenticated user(?: with an? (\w+) role)?$/ do |role|
+Given /^I am an authenticated user with an? (\w+) role$/ do |role|
   visit destroy_user_session_path
   @current_user = User.make(:email => "current_user@example.com", :password => "password", :password_confirmation => "password")
   @current_user.has_role!(role.to_sym) if role
@@ -6,6 +6,15 @@ Given /^I am an authenticated user(?: with an? (\w+) role)?$/ do |role|
     p = Project.make
     @current_user.has_role!(:client, p)
   end
+  visit new_user_session_path
+  step %{I fill in "user_email" with "current_user@example.com"}
+  step %{I fill in "user_password" with "password"}
+  step %{I press "Sign in"}
+end
+
+Given /^I am an authenticated user$/ do
+  visit destroy_user_session_path
+  @current_user = User.make(:email => "current_user@example.com", :password => "password", :password_confirmation => "password")
   visit new_user_session_path
   step %{I fill in "user_email" with "current_user@example.com"}
   step %{I fill in "user_password" with "password"}
