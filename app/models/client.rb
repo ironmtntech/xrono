dc3d40ce5689d28a3ab02786f9bad8d2ed6d2808
Ben Holley <ben@isotope11.com>
2012-03-21 13:34:37 -0500

    [postgres] fix queries in invoices, dashboard controller, and client model

--- app/models/client.rb
+++ app/models/client.rb
@@ -15,9 +15,9 @@ class Client < ActiveRecord::Base
   after_create :create_data_vault
 
   scope :sort_by_name, order('name ASC')
-  scope :active, where('status = "10"')
-  scope :inactive, where('status != "10" AND status != "20"')
-  scope :suspended, where('status = 20')
+  scope :active, where(:status => '10')
+  scope :suspended, where(:status => '20')
+  scope :inactive, where(:status => '30')
   scope :for_user, lambda{|user|
    joins("INNER JOIN projects     p ON p.client_id=clients.id")
    .joins("INNER JOIN roles        r ON r.authorizable_type='Project' AND r.authorizable_id=p.id")
