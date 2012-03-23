6299e37f2bb65f1d25c7a84d18e2844d5536437b
Josh Adams <josh@isotope11.com>
2012-03-23 00:37:59 -0500

    Add totals to weekly time report

--- app/controllers/admin/weekly_time_report_controller.rb
+++ app/controllers/admin/weekly_time_report_controller.rb
@@ -7,6 +7,7 @@ class Admin::WeeklyTimeReportController < ApplicationController
   def show
     redirect_unless_monday('/admin/weekly_time_report/', params[:id])
     @users = User.sort_by_name.select{|u| u.has_role?(:developer) && !u.locked }
+    @weekly_hours_sum = external_hours_for_chart(@users, :date => @start_date)
   end
 
 end
