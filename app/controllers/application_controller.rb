702a303a0e5b84242759d233d2647a996d295edf
Josh Adams <josh@isotope11.com>
2012-04-23 19:07:25 -0500

    Run 7 day rather than 5 day workunit bar graphs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,7 +32,6 @@ class ApplicationController < ActionController::Base
 
     final_array = []
     (start_date..end_date).each do |i_date|
-      next if [6,7].include? i_date.cwday
       _beg, _end = i_date.beginning_of_day, i_date.end_of_day
       hours = WorkUnit.for_users(users).scheduled_between(_beg,_end).all
       final_array << [i_date.strftime("%a"), sum_hours(:external?, hours).to_f, sum_hours(:internal?, hours).to_f]
