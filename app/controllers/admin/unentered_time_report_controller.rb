2c87f6bfd8119194b0de14506e6f8d247eb56647
Adam Gamble <adamgamble@Adams-MacBook-Pro.local>
2012-06-19 15:34:17 -0500

    fixed an issue with the unentered time report

--- app/controllers/admin/unentered_time_report_controller.rb
+++ app/controllers/admin/unentered_time_report_controller.rb
@@ -7,7 +7,7 @@ class Admin::UnenteredTimeReportController < ApplicationController
   def show
     redirect_unless_monday('/admin/unentered_time_report/', params[:id])
     @week_dates = build_week_hash_for(Date.parse(params[:id]))
-    @users = User.unlocked.developers.all
+    @users = User.unlocked.developers.uniq
   end
 
   def load_work_units
