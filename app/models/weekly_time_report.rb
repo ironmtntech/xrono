7ae55192516742377cb387d10740bf812e140490
Josh Adams <josh@isotope11.com>
2012-12-13 08:38:59 -0600

    Permit a partial week in the weekly time report for better diffs-to-date in the dashboard

--- app/models/weekly_time_report.rb
+++ app/models/weekly_time_report.rb
@@ -1,7 +1,8 @@
 # Get internal/external hour split for a given week
 class WeeklyTimeReport
-  def initialize(date_in_week=Time.zone.now)
+  def initialize(date_in_week=Time.zone.now, partial_week=false)
     @date = date_in_week
+    @partial_week = partial_week
   end
 
   def run
@@ -13,7 +14,12 @@ class WeeklyTimeReport
   end
 
   def get_weekly_hours(date)
-    start_date, end_date  = date.beginning_of_week.to_date, date.end_of_week.to_date
+    start_date = date.beginning_of_week.to_date
+    if @partial_week
+      end_date = date
+    else
+      end_date = date.end_of_week.to_date
+    end
     work_units = WorkUnit.scheduled_between(start_date, end_date)
     { external: sum_hours(:external?, work_units).to_i, internal: sum_hours(:internal?, work_units).to_i }
   end
