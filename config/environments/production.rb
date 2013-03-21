0c7c0ea5b4a1ff059f7efbd446a332bcbb31c081
Josh Adams <josh@isotope11.com>
2013-03-21 13:05:23 -0500

    Precompile screen.css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ AssetTrackerTutorial::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( screen.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
@@ -58,4 +58,5 @@ AssetTrackerTutorial::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
 end
