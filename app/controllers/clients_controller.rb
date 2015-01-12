031420225146282fbfa21c3eaf0e9c6f5d5a8965
rclements <rclements@isotope11.com>
2015-01-12 08:38:37 -0600

    added all clients to developer role access

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -7,7 +7,7 @@ class ClientsController < ApplicationController
 
   access_control do
     allow :admin
-    allow :developer, :to => [:index, :inactive_clients, :suspended_clients, :show_complete, :project_subscribe, :project_unsubscribe, :show, :historical_time]
+    allow :developer, :to => [:index, :inactive_clients, :suspended_clients, :show_complete, :project_subscribe, :project_unsubscribe, :show, :historical_time, :all_clients]
 
     action :index do
       allow :developer
